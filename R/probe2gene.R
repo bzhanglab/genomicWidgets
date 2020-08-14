73e413bc641adadcf9f19256ed4223e2cb951bfb
ibphuangchen <ibphuangchen@gmail.com>
2020-08-14 11:13:33 -0500

    fix bug in parallel

--- R/probe2gene.R
+++ R/probe2gene.R
@@ -3,7 +3,7 @@
 #' This function extracts the methylation probes located in the promoter region and CpG islands for each gene and use their methylation levels to summerize the 
 #' gene-level methylation by mean or median.
 #' @param probeMat the probe-level DNA methylation matrix (probe by sample), entries are beta values.
-#' @param nThread the number of threads to be used for the calcluation. If NULL, the number will be equal to the (# of CPU threads)-1. Default is NULL.
+#' @param nThread the number of threads to be used for the calculation. If NULL, no parallel computation will be performed.
 #' @param use specify how the multiple probe values be converted to the single gene-level value. Need to be either 'mean' or 'median'.
 #' @param ... addtional parameter to for makeCluster. If you used a Linux or MacOS, it is proper to add type='FORK'.
 #'
@@ -12,17 +12,21 @@
 #'
 #' @examples hnsccSegfile <- system.file("extdata", "hnscc_GDC_methylation_part.tsv", package = "genomicWidgets")
 #' hnsccMethy <- read.table(file = hnsccSegfile, sep = '\t', row.names = 1, header = TRUE)
-#' probe2gene(probeMat=hnsccMethy)
+#' probe2gene(probeMat=hnsccMethy) #no parallel computation
+#' probe2gene(probeMat=hnsccMethy, nThread = 4) #parallel computation using 4 cores
 probe2gene = function(probeMat, nThread = NULL, use='median', ...){
   if(is.null(nThread))
-    nThread = detectCores() - 1
-  cl = makeCluster(nThread, ...)
-  clusterExport(cl, "probeMat", envir = environment())
+    myLapply = lapply
+  else{
+    cl = makeCluster(nThread, ...)
+    clusterExport(cl, "probeMat", envir = environment())
+    myLapply = function(X, fun) parLapply(cl = cl, X=X, fun=fun)
+  }
   if(use=='median')
-    genelevel = parLapply(epicProbe2GeneMappingClean, function(x)apply(probeMat[x,],2,median,na.rm=T))
+    genelevel = myLapply(epicProbe2GeneMappingClean, function(x)apply(probeMat[x,],2,median,na.rm=T))
   else if(use=='mean')
-    genelevel = parLapply(epicProbe2GeneMappingClean, function(x)apply(probeMat[x,],2,mean,na.rm=T))
+    genelevel = myLapply(epicProbe2GeneMappingClean, function(x)apply(probeMat[x,],2,mean,na.rm=T))
   genelevel = do.call(rbind, genelevel)
-  stop(cl)
+  if(!is.null(nThread)) stopCluster(cl)
   genelevel
 }
